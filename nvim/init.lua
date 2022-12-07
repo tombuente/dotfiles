cca0ab94ba63a9d91fbd9467af5d95ac0dfd4512
Tom Buente <108880623+tombuente@users.noreply.github.com>
2022-12-07 18:34:43 +0100

    Add nvim

---
+++ nvim/init.lua
@@ -0,0 +1,163 @@
+-- PACKER
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require("packer").startup(function(use)
+    -- Packer can manage itself
+    use "wbthomason/packer.nvim"
+
+    use { "dracula/vim", as = "dracula" }
+    use { "https://gitlab.com/madyanov/gruber.vim", as = "gruber" }
+    use { "catppuccin/nvim", as = "catppuccin" }
+
+    use {"williamboman/mason.nvim"}
+    use {"williamboman/mason-lspconfig.nvim"}
+    use {"neovim/nvim-lspconfig"}
+
+    use {"hrsh7th/nvim-cmp"}
+    use {"hrsh7th/cmp-nvim-lsp"}
+
+    use {
+        "nvim-tree/nvim-tree.lua",
+        requires = {"nvim-tree/nvim-web-devicons" -- optional, for file icons
+        },
+        tag = "nightly" -- optional, updated every week. (see issue #1193)
+    }
+end)
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+local cmp = require "cmp"
+cmp.setup({
+    mapping = {
+        ["<C-n>"] = cmp.mapping.select_next_item {
+            behavior = cmp.SelectBehavior.Insert
+        },
+        ["<C-p>"] = cmp.mapping.select_prev_item {
+            behavior = cmp.SelectBehavior.Insert
+        },
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({
+            select = true
+        }),
+        ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ["<S-Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    },
+    sources = cmp.config.sources({{
+        name = "nvim_lsp"
+    }}, {{
+        name = "buffer"
+    }})
+})
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig").clangd.setup {}
+
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha"
+    },
+    transparent_background = false,
+    term_colors = false,
+    dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    styles = {
+        comments = {"italic"},
+        conditionals = {"italic"},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {}
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = false,
+        mini = false
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    }
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "gruber"
+
+vim.o.clipboard = "unnamedplus" -- system clipboard
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+    sort_by = "case_sensitive",
+    view = {
+        adaptive_size = true,
+        mappings = {
+            list = {{
+                key = "u",
+                action = "dir_up"
+            }}
+        }
+    },
+    renderer = {
+        group_empty = true
+    },
+    filters = {
+        dotfiles = true
+    }
+})
+
+vim.wo.number = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
