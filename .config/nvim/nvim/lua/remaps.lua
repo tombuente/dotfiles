a6fca5b3ecc2e61528bffe73053f23a840def55b
Tom Buente <108880623+tombuente@users.noreply.github.com>
2023-06-21 22:58:20 +0200

    Remove nvim

--- .config/nvim/nvim/lua/remaps.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
-vim.keymap.set("n", "<leader>df", vim.diagnostic.open_float)
-
-vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename)
